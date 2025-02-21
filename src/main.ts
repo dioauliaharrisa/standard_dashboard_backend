e2f5c1e2f951564a537c0ed613914d08fe0c2eec
dioauliaharrisa <daharrisa@gmail.com>
2025-02-21 10:05:16 +0700

    Add network interface IP to server log message

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './common/modules/app.module';
+import * as os from 'os';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
+
+  const networkInterfaces = os.networkInterfaces();
+  const ip =
+    Object.values(networkInterfaces)
+      .flat()
+      .find((iface) => iface?.family === 'IPv4' && !iface.internal)?.address ||
+    'localhost';
+
+  Logger.log(
+    `🚀 Server running at http://${ip}:${process.env.PORT ?? 3000}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
